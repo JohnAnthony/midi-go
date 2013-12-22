7699cd59c54620f889c3004078c0aedb53e8f515
John Anthony <john@jo.hnanthony.com>
2013-12-22 06:32:11 +0000

    Added arpegio examples

--- examples/up-and-down-scale-two-tracks.go
+++ examples/up-and-down-scale-two-tracks.go
@@ -12,16 +12,14 @@ func main() {
 	tracks[0] = midi.NewTrack()
 	tracks[1] = midi.NewTrack()
 
-	tracks[1].AddEvent(1, midi.NoteOn(1, 48, 0xff))
-
 	for _, v := range cmajscaleup {
-		tracks[0].AddEvent(2, midi.NoteOn(0, v, 0xff))
-		tracks[1].AddEvent(2, midi.NoteOn(1, 48, 0xff))
+		tracks[0].AddEvent(1, midi.NoteOn(0, v, 0xff))
+		tracks[1].AddEvent(1, midi.NoteOn(0, 48, 0xff))
 	}
 	
 	for _, v := range cmajscaledown {
-		tracks[0].AddEvent(2, midi.NoteOn(0, v, 0xff))
-		tracks[1].AddEvent(2, midi.NoteOn(1, 48, 0xff))
+		tracks[0].AddEvent(1, midi.NoteOn(0, v, 0xff))
+		tracks[1].AddEvent(1, midi.NoteOn(0, 48, 0xff))
 	}
 
 	midi.WriteOut("test.midi", midi.Syncronous, 4, tracks)
