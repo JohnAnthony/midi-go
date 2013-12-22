b577f508a408d661575f6977cf1ea2dfba56be9d
John Anthony <john@jo.hnanthony.com>
2013-12-22 03:06:04 +0000

    First release

---
+++ examples/up-and-down-scale.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/JohnAnthony/midi-go"
+)
+
+func main() {
+	cmajscaleup := []uint8 {60, 62, 64, 65, 67, 69, 71, 72}
+	cmajscaledown := []uint8 {72, 71, 69, 67, 65, 64, 62, 60}
+	
+	tracks := make([]midi.Track, 1)
+	tracks[0] = midi.NewTrack()
+
+	for _, v := range cmajscaleup {
+		tracks[0].AddEvent(2, midi.NoteOn(0, v, 0xff))
+		tracks[0].AddEvent(2, midi.NoteOff(0, v, 0xff))
+	}
+	
+	for _, v := range cmajscaledown {
+		tracks[0].AddEvent(2, midi.NoteOn(0, v, 0xff))
+		tracks[0].AddEvent(2, midi.NoteOff(0, v, 0xff))
+	}
+
+	midi.WriteOut("test.midi", 0, 4, tracks)
+}
